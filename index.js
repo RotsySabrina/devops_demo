8c3d751635be14e689d3973bcc2fbbc92c206fac
RotsySabrina <RotsySabrina>
2025-12-17 13:32:54 +0300

    Ajout serveur Express

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
 function addition(a, b){
     return a + b;
 }
 
-console.log("Résultat :", addition(2, 3));
+const result = addition(2, 3);
+console.log("Résultat :", result);
 
-module.exports = addition;
+// route principale
+app.get("/", (req, res) => {
+  res.send(`Résultat de l'addition : ${result}`);
+});
+
+// démarrer le serveur
+app.listen(port, () => {
+  console.log(`Serveur actif sur le port ${port}`);
+});
